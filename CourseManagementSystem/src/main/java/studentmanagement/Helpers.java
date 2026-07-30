7e3595b6949aa0f4e72f6be88c332d988d9c2a78
Hajar Alhajri <enghajaralhajri@outlook.com>
2026-07-30 21:37:56 +0300

    initial commit

---
+++ CourseManagementSystem/src/main/java/studentmanagement/Helpers.java
@@ -0,0 +1,61 @@
+
+package studentmanagement;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Helpers {
+
+    static ArrayList<Student> students = new ArrayList<>();
+
+    public void addStudent(Student s) {
+        if (s != null) {
+            students.add(s);
+        }
+    }
+
+    public static void writeFile(ArrayList<Student> studentsList) {
+
+        try {
+            String fileName = "students.txt";
+
+            FileOutputStream fileOut = new FileOutputStream(fileName);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+            out.writeObject(studentsList);
+
+            out.close();
+            fileOut.close();
+
+            System.out.println("Students saved successfully.");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static ArrayList<Student> readFile() {
+
+        ArrayList<Student> students = new ArrayList<>();
+
+        try {
+           String fileName = "students.txt";
+
+            FileInputStream fileIn = new FileInputStream(fileName);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+
+            students = (ArrayList<Student>) in.readObject();
+
+            in.close();
+            fileIn.close();
+
+            System.out.println("Students loaded successfully.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return students;
+    }
+}
